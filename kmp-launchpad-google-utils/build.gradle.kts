a038ef8189eac64fdddad32d09a7e93ea85752b8
Eric Emery <eric.emery@bottlerocketstudios.com>
2024-03-04 12:16:09 -0600

    ASAA-192 - Create `LaunchPad-Google-UI` library

---
+++ kmp-launchpad-google-utils/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kt.lint.gradle)
+    `maven-publish`
+}
+
+kotlin {
+    applyDefaultHierarchyTemplate()
+    androidTarget {
+        publishLibraryVariants("release")
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.content.negotiation)
+            implementation(libs.ktor.logging)
+            implementation(libs.ktor.serialization.json)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+    }
+}
+
+android {
+    namespace = "com.bottlerocketstudios.launchpadgoogleutils"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+ktlint {
+    ignoreFailures.set(true)
+    android.set(true)
+
+    filter {
+        exclude("**/generated/**")
+    }
+}
+
+group = extra["publishing.group"] as String
+version = libs.versions.launchpad.google.utils.get()
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/BottleRocketStudios/KMP-LaunchPad-GoogleUtils")
+            credentials {
+                username = System.getenv("REPO_READ_WRITE_USER")
+                password = System.getenv("REPO_READ_WRITE_TOKEN")
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LaunchPad_Google_Utils"
+include(":kmp-launchpad-google-utils")
