30d499ba3292f891b96197eba72f8162f030ab07
Eric Emery <eric.emery@bottlerocketstudios.com>
2024-04-24 15:43:18 -0500

    ASAA-207 - Small changes to match the IgniteLocality autocomplete

--- kmp-launchpad-google-utils/src/commonMain/kotlin/com/bottlerocketstudios/launchpad/google/utils/model/place/AutocompleteResult.kt
+++ kmp-launchpad-google-utils/src/commonMain/kotlin/com/bottlerocketstudios/launchpad/google/utils/model/place/AutocompleteResult.kt
@@ -3,8 +3,8 @@ package com.bottlerocketstudios.launchpad.google.utils.model.place
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PlaceAutocompleteResultModel(
+data class AutocompleteResult(
+    val placeId: String,
     val primaryText: String,
-    val secondaryText: String,
-    val placeId: String
+    val secondaryText: String
 )
